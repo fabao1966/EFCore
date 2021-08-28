b56bbe51821027c61b2e332d5cfa00fd6420eb54
fabio soares <fabao1966@yahoo.com.br>
2021-08-28 10:08:56 -0300

    Add project files.

---
+++ EFCore.Legado/Batalha.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFCore.Legado
+{
+    public partial class Batalha
+    {
+        public Batalha()
+        {
+            HeroisBatalhas = new HashSet<HeroisBatalha>();
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime IniciadoEm { get; set; }
+        public DateTime FinalizadoEm { get; set; }
+
+        public virtual ICollection<HeroisBatalha> HeroisBatalhas { get; set; }
+    }
+}
---
+++ EFCore.Legado/HeroAppContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EFCore.Legado
+{
+    public partial class HeroAppContext : DbContext
+    {
+        public HeroAppContext()
+        {
+        }
+
+        public HeroAppContext(DbContextOptions<HeroAppContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Arma> Armas { get; set; }
+        public virtual DbSet<Batalha> Batalhas { get; set; }
+        public virtual DbSet<Heroi> Herois { get; set; }
+        public virtual DbSet<HeroisBatalha> HeroisBatalhas { get; set; }
+        public virtual DbSet<IdentidadesSecreta> IdentidadesSecretas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Password=12345678;Persist Security Info=True;User ID=fabao1966;Initial Catalog=HeroApp;Data Source=WINDOWS10\\SQLEXPRESS");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Arma>(entity =>
+            {
+                entity.HasIndex(e => e.HeroiId, "IX_Armas_HeroiId");
+
+                entity.HasOne(d => d.Heroi)
+                    .WithMany(p => p.Armas)
+                    .HasForeignKey(d => d.HeroiId);
+            });
+
+            modelBuilder.Entity<HeroisBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+
+                entity.HasIndex(e => e.HeroiId, "IX_HeroisBatalhas_HeroiId");
+
+                entity.HasOne(d => d.Batalha)
+                    .WithMany(p => p.HeroisBatalhas)
+                    .HasForeignKey(d => d.BatalhaId);
+
+                entity.HasOne(d => d.Heroi)
+                    .WithMany(p => p.HeroisBatalhas)
+                    .HasForeignKey(d => d.HeroiId);
+            });
+
+            modelBuilder.Entity<IdentidadesSecreta>(entity =>
+            {
+                entity.HasIndex(e => e.HeroiId, "IX_IdentidadesSecretas_HeroiId")
+                    .IsUnique();
+
+                entity.HasOne(d => d.Heroi)
+                    .WithOne(p => p.IdentidadesSecreta)
+                    .HasForeignKey<IdentidadesSecreta>(d => d.HeroiId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
